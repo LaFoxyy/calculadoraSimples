b3d563b4f6b8d4f9b800a5fd26d6684c1d502618
LaFoxyy <dr4caryslolpro@gmail.com>
2025-08-05 14:17:05 -0300

    Add files via upload

---
+++ Calculadora-cliente/build/classes/calc/cliente/model/Mensagem.rs
@@ -0,0 +1,1 @@
+calc.socket.model.Mensagem
---
+++ Calculadora-clienteStress/src/calc/cliente/util/Util.java
@@ -0,0 +1,44 @@
+package calc.cliente.util;
+
+public class Util {
+
+    public static boolean verificaOperador(String calculo){
+
+        if(calculo.contains("+") ||
+                calculo.contains("-") ||
+                calculo.contains("*") ||
+                calculo.contains("/") ||
+                calculo.contains("√") ||
+                calculo.contains("^")){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String getOperador(String calculo){
+        if (calculo.contains("+")) {
+            return "+";
+        }
+        if (calculo.contains("-")) {
+            return "-";
+        }
+        if (calculo.contains("*")) {
+            return "*";
+        }
+        if (calculo.contains("/")) {
+            return "/";
+        }
+        if (calculo.contains("√")) {
+            return "√";
+        }
+        if (calculo.contains("^")) {
+            return "^";
+        }
+        return null;
+    }
+
+    public static String ponto2virguta(String s){
+        return s.replaceAll(".", ",");
+    }
+}
